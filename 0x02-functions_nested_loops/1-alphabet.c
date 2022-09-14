81451180031b9770effd969278bbcdacdb655051
lenalawrence <lawrencelena@rocketmail.com>
2022-09-13 18:39:56 -0700

    first

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+/*
+ * File: 1-alphabet.c
+ *
+ * Auth:lena
+ */
+#include "main.h"
+/**
+ * print_alphabet - prints the alphabet in lowercase, followed by a new line.
+ */
+void print_alphabet(void)
+{
+	char alpha;
+
+	for (alpha = 97; alpha <= 122; alpha++)
+		_putchar(alpha);
+	_putchar('\n');
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+Reame file for 0x02-functions_nested_loops
